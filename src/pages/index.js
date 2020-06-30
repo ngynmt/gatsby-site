483d51d82910651c48f58baa4f0865311b0e91c7
Michelle Nguyen <michelle.nguyen@potatolondon.com>
2020-06-30 15:21:47 -0700

    Add email easter egg

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,27 +12,29 @@ export default function Home() {
   return (
     <div class="meesh-app">
       <div class="meesh-home">
-        <h1>hi <span role="img" aria-label="emoji hand wave">👋</span> i'm michelle</h1>
+        <h1>hi <div class="meesh-home__hello">
+            <a href="mailto:ngynmt@gmail.com?subject=Hello! 👋"><span role="img" aria-label="emoji hand wave">👋</span></a>
+          </div> i'm michelle.</h1>
           <div class="meesh-home__social">
             <div class="meesh-home__social-row">
-              <a href='https://gitlab.com/ngynmt' className="meesh-home__social-icon">
+              <a href='https://gitlab.com/ngynmt' class="meesh-home__social-icon">
                 <span><FontAwesomeIcon icon={faGitlab} size="lg" aria-hidden="true"/></span>
               </a>
-              <a href='https://github.com/ngynmt' className="meesh-home__social-icon">
+              <a href='https://github.com/ngynmt' class="meesh-home__social-icon">
                 <span><FontAwesomeIcon icon={faGithub} size="lg" aria-hidden="true"/></span>
               </a>
-              <a href='https://linkedin.com/in/nguyenchelle' className="meesh-home__social-icon">
+              <a href='https://linkedin.com/in/nguyenchelle' class="meesh-home__social-icon">
                 <span><FontAwesomeIcon icon={faLinkedin} size="lg" aria-hidden="true"/></span>
               </a>
             </div>
             <div class="meesh-home__social-row">
-              <a href='https://twitter.com/ngynmt' className="meesh-home__social-icon">
+              <a href='https://twitter.com/ngynmt' class="meesh-home__social-icon">
                 <span><FontAwesomeIcon icon={faTwitter} size="lg" aria-hidden="true"/></span>
               </a>
-              <a href='https://medium.com/@ngynmt' className="meesh-home__social-icon">
+              <a href='https://medium.com/@ngynmt' class="meesh-home__social-icon">
                 <span><FontAwesomeIcon icon={faMediumM} size="lg" aria-hidden="true"/></span>
               </a>
-              <a href='https://keybase.io/ngynmt/chat' className="meesh-home__social-icon">
+              <a href='https://keybase.io/ngynmt/chat' class="meesh-home__social-icon">
                 <span><FontAwesomeIcon icon={faKeybase} size="lg" aria-hidden="true"/></span>
               </a>
             </div>
